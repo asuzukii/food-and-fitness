b774bcaa88043622355e9850e98cffac6d592af3
asuzukii <asuzuki.swe@gmail.com>
2023-07-19 23:46:33 -0700

    more probable bug fix

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -72,8 +72,7 @@ export const createNewProject = async (form: ProjectForm, creatorId: string, tok
   }
 };
 
-export const fetchAllProjects = async (category?: string | null, endCursor?: string 
-   null) => {
+export const fetchAllProjects = async (category?: string | null, endCursor?: string | null) => {
   client.setHeader("x-api-key", apiKey);
   return makeGraphQLRequest(projectsQuery, { category, endCursor });
 };
